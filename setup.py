d367b058cb5634a77cd30d5318985fe78fcf53d4
Matt Riley <spam@oscmp.com>
2015-05-30 00:22:55 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "web_plugins",
+    version = "0.0.1",
+    author = "Matt Riley",
+    author_email = "mattriley@oscmp.com",
+    description = "Utilities to build python webapps",
+    license = "BSD",
+    keywords = "Utilities to build python webapps",
+    url = "http://www.oscmp.com/web_plugins",
+    packages=['web_plugins'],
+    long_description='README',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
