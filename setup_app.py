0f7722019653c20c35c29267f7de378e59d740ea
Matt Riley <spam@oscmp.com>
2015-06-03 23:43:42 -0400

    Reorganize file a bit

--- setup_app.py
+++ setup_app.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python
 
-
-from distutils.util import strtobool
-import readline
-import os
-
-
 import argparse
+from distutils.util import strtobool
+import os
+import readline
 
-parser = argparse.ArgumentParser(description='Setup web plugins app.')
-parser.add_argument('--generate-basic-site', dest='generate_basic_site', action='store_true')
-parser.add_argument('--no-generate-basic-site', dest='generate_basic_site', action='store_false')
-parser.set_defaults(generate_basic_site=True)
+def write_file(filename, content):
+	with open(filename, 'w') as text_file:
+		text_file.write(content)
 
-parser.add_argument('--app-name', dest='app_name', default='app')
-
-args = parser.parse_args()
-
-app_name = args.app_name
-generate_basic_site = args.generate_basic_site
-
-
-
-working_directory = os.getcwd()
+def write_executable(filename, content):
+	write_file(filename, content)
+	os.system('chmod +x ' + filename)
 
 def input_or_default(prompt, default=''):
 	if (default != ''):
@@ -30,24 +19,43 @@ def input_or_default(prompt, default=''):
 	prompt = prompt + ': '
 	return raw_input(prompt).strip() or default
 
+def get_arguments():
+	parser = argparse.ArgumentParser(description='Setup web plugins app.')
+	parser.add_argument('--generate-basic-site', dest='generate_basic_site', action='store_true')
+	parser.add_argument('--no-generate-basic-site', dest='generate_basic_site', action='store_false')
+	parser.set_defaults(generate_basic_site=True)
+	parser.add_argument('--app-name', dest='app_name', default='app')
+	args = parser.parse_args()
+	return args
+
+args = get_arguments()
+app_name = args.app_name
+generate_basic_site = args.generate_basic_site
+
+working_directory = os.getcwd()
+
+######### Get info from user #############
 app_name = input_or_default('Enter App Name', app_name)
 host_name = input_or_default('Enter Host Name', app_name + '.oscmp.com')
 nginx_config_location = input_or_default('Nginx Config Location', '/etc/nginx/sites-enabled')
 restart_nginx_command = input_or_default('Nginx Restart Command', 'sudo service nginx restart')
 add_to_host_file = input_or_default('Add to host file', 'y')
 add_to_upstart = input_or_default('Add to upstart', 'y')
-run_server_template = """source virtual_env/bin/activate
+##########################################
+
+
+
+####### File Templates ##################
+run_server = """source virtual_env/bin/activate
 uwsgi --http 127.0.0.1:8001 --wsgi-file {}.py  --honour-stdin --async 10 
 """.format(app_name)
 
-run_server_nginx_template = """cd {working_directory}
+run_server_nginx = """cd {working_directory}
 source virtual_env/bin/activate
 uwsgi --socket virtual_env/{app_name}.sock --wsgi-file {app_name}.py --chmod-socket=666 --async 10
 """.format(app_name=app_name, working_directory=working_directory)
 
-
-
-nginx_config_template ="""
+nginx_config ="""
 upstream web_plugin_app_{app_name}{{
     server unix://{working_directory}/virtual_env/{app_name}.sock; # for a file socket
 }}
@@ -88,7 +96,7 @@ server {{
 }}
 """.format(app_name=app_name, working_directory=working_directory)
 
-basic_site_template = """
+basic_site = """
 import web_plugins.app
 from web_plugins.app import application
 from web_plugins.response import HtmlResponse
@@ -102,7 +110,7 @@ def {app_name}(request):
 application.handler = {app_name}
 """.format(app_name=app_name)
 
-git_ignore_template = """
+git_ignore = """
 virtual_env/
 *.pyc
 run_server
@@ -110,33 +118,6 @@ run_server_nginx
 *.conf
 cleanup.sh
 """
-
-
-def write_file(filename, content):
-	with open(filename, 'w') as text_file:
-		text_file.write(content)
-def write_executable(filename, content):
-	write_file(filename, content)
-	os.system('chmod +x ' + filename)
-
-
-write_executable('run_server_nginx', run_server_nginx_template)
-write_executable('run_server', run_server_template)
-write_file('{}_nginx.conf'.format(app_name), nginx_config_template)
-write_file('.gitignore', git_ignore_template)
-
-if generate_basic_site:
-	write_executable('{}.py'.format(app_name), basic_site_template.format(app_name=app_name))
-
-
-os.system('sudo ln -s {0}/{1}_nginx.conf {2}/{1}_nginx.conf'.format(working_directory, app_name, nginx_config_location))
-os.system(restart_nginx_command)
-
-host_line = '127.0.0.1 ' + host_name
-
-if strtobool(add_to_host_file):
-	os.system('echo "' + host_line + '" | sudo tee -a /etc/hosts');
-
 
 upstart_config = """description "Handles Web Plugin {app_name}"
 start on runlevel [2345]
@@ -146,6 +127,40 @@ script
 	exec bash -c {working_directory}/run_server_nginx
 end script
 """.format(app_name=app_name, working_directory=working_directory)
+
+cleanup_script_template = """
+rm {app_name}_nginx.conf
+rm {app_name}.py
+rm run_server
+rm run_server_nginx
+rm -rf virtual_env
+sudo rm {nginx_config_location}/{app_name}_nginx.conf
+sudo sed -i '/{host_line}/d' /etc/hosts
+rm {upstart_config_file_name}
+{upstart_line}
+rm cleanup.sh 
+"""
+###################################################
+
+
+############  Write Files  #########################
+write_executable('run_server_nginx', run_server_nginx)
+write_executable('run_server', run_server)
+write_file('{}_nginx.conf'.format(app_name), nginx_config)
+write_file('.gitignore', git_ignore)
+
+if generate_basic_site:
+	write_executable('{}.py'.format(app_name), basic_site.format(app_name=app_name))
+
+
+os.system('sudo ln -s {0}/{1}_nginx.conf {2}/{1}_nginx.conf'.format(working_directory, app_name, nginx_config_location))
+os.system(restart_nginx_command)
+
+host_line = '127.0.0.1 ' + host_name
+if strtobool(add_to_host_file):
+	os.system('echo "' + host_line + '" | sudo tee -a /etc/hosts');
+
+
 
 use_upstart = strtobool(add_to_upstart)
 if use_upstart:
@@ -158,19 +173,8 @@ if use_upstart:
 
 os.system("sed -i -e 's/^setup_app.py$/setup_app.py --no-generate-basic-site --app-name={app_name}/g' bootstrap.sh".format(app_name=app_name))
 
-cleanup_script = """
-rm {app_name}_nginx.conf
-rm {app_name}.py
-rm run_server
-rm run_server_nginx
-rm -rf virtual_env
-sudo rm {nginx_config_location}/{app_name}_nginx.conf
-sudo sed -i '/{host_line}/d' /etc/hosts
-rm {upstart_config_file_name}
-{upstart_line}
-rm cleanup.sh 
-""".format(app_name=app_name, host_name=host_name, nginx_config_location=nginx_config_location, host_line=host_line, 
-           upstart_config_file_name=upstart_config_file_name,
-		   upstart_line='sudo rm /etc/init/' + upstart_config_file_name if use_upstart else '')
 
-write_executable('cleanup.sh', cleanup_script)
+
+write_executable('cleanup.sh', cleanup_script_template.format(app_name=app_name, host_name=host_name, nginx_config_location=nginx_config_location, host_line=host_line, 
+           upstart_config_file_name=upstart_config_file_name,
+		   upstart_line='sudo rm /etc/init/' + upstart_config_file_name if use_upstart else ''))
